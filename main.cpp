7ef64f05f643785da793645049e5868ffb236ca6
Sam Brown <sam.f.brown.89@gmail.com>
2014-06-08 01:56:54 +0100

    Added a bit of printing to main

--- main.cpp
+++ main.cpp
@@ -29,29 +29,35 @@ int main(int argc, char *argv[]){
 		cerr<<"Error opening file "<<argv[1]<<endl;
 		exit(EXIT_FAILURE);
 	}
+
+	cout << "Reading file " << argv[1] << endl;
 	read_xyzclcpcs(infile,allPoints);
 
 
-	// Put all points with cl>threshold in a sausage
+	// Put all points with cl<threshold in a sausage
+	cout << "Thresholding, sausages have cl<" << threshold_level << endl;
 	threshold(allPoints,threshold_level);
 
 	// Count the sausages
+	cout << "Pixel counts in (0) or not in (1) a sausage:" << endl;
 	vector<int> sausage_count=count_sausages(allPoints);
 	for (size_t i=0; i<sausage_count.size(); i++){
 		cout << i << " " << sausage_count[i] << endl;
-		cout << endl;
 	}
 
 	// Link the points to their neighbours
+	cout << "Linking pixels..." << endl;
 	neighLink_xyzclcpcs(allPoints);
 
 	// Check the neighbours
 	//printAllNeighs(allPoints);
 
 	// Separate the points into separate, contiguous sausages
+	cout << "Distinguishing sausages..." << endl;
 	flood_fill(allPoints);
 
 	// Count the sausages
+	cout << "Sausage sizes:" << endl;
 	sausage_count=count_sausages(allPoints);
 	for (size_t i=0; i<sausage_count.size(); i++){
 		cout << i << " " << sausage_count[i] << endl;
